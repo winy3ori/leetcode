05a67c67c3dc4e9a6bb6c4680f884aecf7c30b34
y3ori <154998887+winy3ori@users.noreply.github.com>
2026-05-08 14:47:15 +0900

    Time: 13 ms (28.31%), Space: 46.6 MB (23.55%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        if(s.length() != t.length()) return false;
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for(char c : s.toCharArray()){
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        for(char c : t.toCharArray()){
+            map.put(c, map.getOrDefault(c, 0) - 1);
+        }
+
+        for(int x : map.values()){
+            if(x != 0) return false;
+        }
+
+        return true;
+
+    }
+}
