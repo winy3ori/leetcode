2d7d94df2e9c475252be8900a346404332fa4006
y3ori <154998887+winy3ori@users.noreply.github.com>
2026-05-08 14:50:57 +0900

    Time: 6 ms (99.53%), Space: 49.4 MB (91.68%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        
+        HashMap<String, List<String>> map = new HashMap<>();
+
+        for(String str : strs){
+            char[] arr = str.toCharArray();
+            Arrays.sort(arr);
+            String sorted = new String(arr);
+
+            map.putIfAbsent(sorted, new ArrayList<>());
+            map.get(sorted).add(str);
+        }
+
+        return new ArrayList<>(map.values());
+
+    }
+}
