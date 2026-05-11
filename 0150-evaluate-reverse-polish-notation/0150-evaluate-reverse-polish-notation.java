c578caa9708055721b7fb9abf7cdc6e1796e6512
y3ori <154998887+winy3ori@users.noreply.github.com>
2026-05-11 17:07:04 +0900

    Time: 6 ms (91.32%), Space: 45.3 MB (61.39%) - LeetHub

---
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+
+        Stack<Integer> stack = new Stack<>();
+
+        for(String token : tokens){
+
+            switch(token){
+
+                case "+":
+                    stack.push(stack.pop() + stack.pop());
+                    break;
+                case "-":{
+                    int a = stack.pop();
+                    int b = stack.pop();
+                    stack.push(b-a);
+                    break;
+                }
+                case "*":
+                    stack.push(stack.pop() * stack.pop());
+                    break;
+                case "/":{
+                    int a = stack.pop();
+                    int b = stack.pop();
+                    stack.push(b/a);
+                    break;
+                }
+                default:
+                    stack.push(Integer.parseInt(token));
+            }
+        }
+
+        return stack.pop();
+        
+    }
+}
