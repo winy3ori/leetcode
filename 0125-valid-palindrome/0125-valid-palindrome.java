ef1fe3e9e18376e14b5d636d99377a8c4a9474b8
y3ori <154998887+winy3ori@users.noreply.github.com>
2026-05-18 17:17:35 +0900

    Time: 14 ms (30.92%), Space: 46.3 MB (30.55%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+
+        s = s.toUpperCase().replaceAll("[^A-Z0-9]", "");
+        int left = 0; 
+        int right = s.length()-1;
+
+        while (left < right){
+
+            if (s.charAt(left) != s.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+
+        }
+
+        return true;
+
+    }
+}
