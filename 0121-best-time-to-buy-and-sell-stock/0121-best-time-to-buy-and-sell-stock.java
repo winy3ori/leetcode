7832a95a800f6758d95be5c5ef578014ff1187b6
y3ori <154998887+winy3ori@users.noreply.github.com>
2026-05-26 16:48:13 +0900

    Time: 1 ms (99.93%), Space: 94.6 MB (31.39%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+
+        int left = 0;
+        int right = 1;
+        int profit = 0;
+
+        while (right < prices.length){
+            if (prices[left] < prices[right]){
+                profit = Math.max(profit, prices[right] - prices[left]);
+            } else{
+                left = right;
+            }
+            right++;
+        }
+
+        return profit;
+        
+    }
+}
